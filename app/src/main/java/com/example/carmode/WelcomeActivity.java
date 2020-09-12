04a329e69c0dc996cb7964a990d7c9289c4fdadd
MrThanasiz <thanasiz99@gmail.com>
2020-09-12 11:25:39 +0200

    Added application launching button by package name (facebook as example)

--- app/src/main/java/com/example/carmode/WelcomeActivity.java
+++ app/src/main/java/com/example/carmode/WelcomeActivity.java
@@ -1,10 +1,12 @@
 package com.example.carmode;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.carmode.MESSAGE";
@@ -21,5 +23,22 @@ public class WelcomeActivity extends AppCompatActivity {
         String message = "passing a msg";
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+    // Test code for running other apps by package name
+
+    public void runApp(View view) {
+        String pkgName = "com.facebook.katana";
+        startNewActivity(this, pkgName);
+    }
+
+    public void startNewActivity(Context context, String packageName) {
+        Intent intent = context.getPackageManager().getLaunchIntentForPackage(packageName);
+        if (intent == null) {
+            // Bring user to the market or let them choose an app?
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("market://details?id=" + packageName));
+        }
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
     }
 }
